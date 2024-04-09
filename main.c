39e5b04fd3deb4a9d66df81462365f9d4d916423
stephen smithstone <ssmithstone@me.com>
2024-04-10 04:57:25 +1000

    Add time details to output
    A struct is now utilized to get time details from the 'now' variable in main.c. These details, which include the day of the year, day of the week, year, month, day of the month, hour, minute, and seconds, are then printed out.

--- main.c
+++ main.c
@@ -18,6 +18,19 @@ void print_current_time() {
     printf("The computer thinks it's : %ld\n" , now); // prints the (number of seconds )epoc time since 1/1/1970
     printf("Formatted time : %s\n" , ctime(&now)); // default formatted time.
 
+    struct tm *clock;
+    clock = localtime(&now);
+    puts("Time details:");
+    printf(" Day of the year: %d\n", clock->tm_yday);
+    printf(" Day of the week: %d\n", clock->tm_wday);
+    printf("            year: %d\n", clock->tm_year+1900);
+    printf("           month: %d\n", clock->tm_mon+1);
+    printf("Day of the month: %d\n", clock->tm_mday);
+    printf("            hour: %d\n", clock->tm_hour);
+    printf("          minute: %d\n", clock->tm_min);
+    printf("         seconds: %d\n", clock->tm_sec);
+
+
 }
 
 void print_greeting(int argc, char *const *argv) {
