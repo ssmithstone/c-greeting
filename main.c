b03fc3d86fabf305954728e230f915178f99ef72
stephen smithstone <ssmithstone@me.com>
2024-04-10 04:47:20 +1000

    Add print_current_time and print_greeting functions
    The code has been updated to include two new functions: print_current_time and print_greeting. print_current_time function retrieves and displays the current system time, while print_greeting function generates a greeting message. Both functions are now being called in the main function.

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <time.h>
+
+void print_greeting(int argc, char *const *argv);
+
+void print_current_time();
 
 int main(int argc , char ** argv) {
+
+    print_current_time();
+    print_greeting(argc, argv);
+    return 0;
+}
+
+void print_current_time() {
+    time_t now; // create var to store time in
+    time(&now); // store the time into the address of now, example of function that accepts pointer to modify value. (non-pure function)
+    printf("The computer thinks it's : %ld\n" , now); // prints the (number of seconds )epoc time since 1/1/1970
+    printf("Formatted time : %s\n" , ctime(&now)); // default formatted time.
+
+}
+
+void print_greeting(int argc, char *const *argv) {
     if(argc < 2){
         const char * name = "Joe Blogs";
         printf("Hello, %s\n" , name );
@@ -8,5 +28,4 @@ int main(int argc , char ** argv) {
     else {
         printf("Hello, %s\n" , argv[1]);
     }
-    return 0;
 }
