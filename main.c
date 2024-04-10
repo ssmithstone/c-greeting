61be60484381fc3768b928ba25d0c83744c1224c
stephen smithstone <ssmithstone@me.com>
2024-04-10 19:11:04 +1000

    Refactor time printing functions in main.c
    Updated the time printing functions in main.c for clearer code structure. These updates include modifying the print_current_time function to take a time_t pointer, adding a new function (print_current_time_using_formating_func) for formatted time printing and calling these functions in the main function. Now we get time only once and use it multiple times.

--- main.c
+++ main.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 void print_greeting(int argc, char *const *argv);
 
-void print_current_time();
+void print_current_time(const time_t* now);
+
+void print_current_time_using_formating_func(const time_t* now){
+    struct tm *clock;
+    clock = localtime(now);
+    char time_string[64];
+    strftime(time_string, sizeof(time_string), "%Y-%m-%d %H:%M:%S", clock);
+    printf("Date formatted using strftime %s\n" , time_string);
+}
 
 int main(int argc , char ** argv) {
+    time_t now; // create var to store time in
+    time(&now); // store the time into the address of now, example of function that accepts pointer to modify value. (non-pure function)
 
-    print_current_time();
+    print_current_time(&now);
+    print_current_time_using_formating_func(&now);
     print_greeting(argc, argv);
     return 0;
 }
 
-void print_current_time() {
-    time_t now; // create var to store time in
-    time(&now); // store the time into the address of now, example of function that accepts pointer to modify value. (non-pure function)
-    printf("The computer thinks it's : %ld\n" , now); // prints the (number of seconds )epoc time since 1/1/1970
-    printf("Formatted time : %s\n" , ctime(&now)); // default formatted time.
+
+
+
+
+void print_current_time(const time_t* now) {
+    printf("The computer thinks it's : %ld\n" , *now); // prints the (number of seconds )epoc time since 1/1/1970
+    printf("Formatted time : %s\n" , ctime(now)); // default formatted time.
 
     struct tm *clock;
-    clock = localtime(&now);
+    clock = localtime(now);
     puts("Time details:");
     printf(" Day of the year: %d\n", clock->tm_yday);
     printf(" Day of the week: %d\n", clock->tm_wday);
